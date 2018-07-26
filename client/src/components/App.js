9ff8d14ae42fdc530980d8430262622d684de1fc
Chidt12 <vipmath171@gmail.com>
2018-07-26 21:17:37 +0700

    26/7/2018 chidt

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
+
+import TextTest from './TextTest';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-         <h1>Nothing</h1>
+         <BrowserRouter>
+            <div>
+              <Route exact path="/texttest" component={TextTest} />
+            </div>
+         </BrowserRouter>
       </div>
     );
   }
