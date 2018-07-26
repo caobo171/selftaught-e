cc4ca6e685a305209454ad819062bb052478a6e2
vipmath171 <vipmath171course@gmail.com>
2018-07-27 05:56:23 +0700

    complete login in facebook and google

--- configs/dev.js
+++ configs/dev.js
@@ -2,5 +2,7 @@ module.exports={
     mongoURI:'mongodb://caobo171:tuilacao171@ds231941.mlab.com:31941/selftaught-e-dev',
     googleClientID : '618758307731-date3rerqekghcp71ku2dtmpg202d0r4.apps.googleusercontent.com',
     googleClientSecret: 'Xcgk7yG_cs1u8OFTxCA_OTwT',
+    facebookClientID : '1782669071819867',
+    facebookClientSecret : '96e89207538deda6baff8a2a7740b8b9',
     cookieKey:'caoboblablab',
 }
--- index.js
+++ index.js
@@ -3,23 +3,40 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const keys = require('./configs/keys');
 const socket = require('socket.io');
+const passport = require('passport');
+const cookieSession = require('cookie-session');
 
+require('./models/User');
 
-console.log(keys.mongoURI);
 mongoose.connect(keys.mongoURI,
     { useNewUrlParser: true }
     , () => {
         console.log('database connect');
     })
+
+require('./services/passport');
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
-if(process.env.NODE_ENV === 'production'){
+app.use(bodyParser.json());
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoute')(app);
+
+if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
     const path = require('path');
-    app.get('*',(req, res)=>{
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
 
@@ -29,6 +46,6 @@ server.listen(port, () => {
 })
 
 
-io.on('connection',(socket)=>{
+io.on('connection', (socket) => {
     console.log('helloo sockeetttt');
 })
