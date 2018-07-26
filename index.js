c0db6e24cf21d6b790b08bea30d50219b9751733
vipmath171 <vipmath171course@gmail.com>
2018-07-26 20:45:54 +0700

    fix deployment errors

--- index.js
+++ index.js
@@ -17,13 +17,11 @@ const io = require('socket.io')(server);
 
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'));
-
     const path = require('path');
-    app.get('*',(req,res)=>{
-       res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    app.get('*',(req, res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
     });
 }
-
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
