3588740333a68324d6ed65b774c85c3bb4b12651
Long <toivabanlaban@gmail.com>
2018-07-29 21:38:27 +0700

    < add p5 va ve canvas ket noi socket.io connection ko dk, sua cai footer danh van sai >

---
+++ client/src/components/game/Game.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react';
+import P5Wrapper from 'react-p5-wrapper';
+import sketch from './sketch';
+import io from 'socket.io-client';
+
+class Game extends Component {    
+/*     constructor(props){
+        super(props);
+        this.state ={
+            socket : null,
+        };
+    }
+
+    componentWillMount(){
+        this.initSocket();
+    }
+
+    initSocket = () => {
+        const socket = io.connect('http://localhost/5000');
+        console.log(socket);
+        socket.on("connection", () => {
+            console.log('connected');
+        })
+        this.setState({socket});
+    } */
+    
+    render(){ 
+        return( 
+            <div>
+                <P5Wrapper sketch={sketch}/>
+            </div>
+        );
+    }   
+}
+
+export default Game;
